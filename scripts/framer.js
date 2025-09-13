84a4efa422fcfa1f5c268192a360c05432299029
Etwin Feoh <xcode6six@gmail.com>
2025-09-13 15:25:51 +0800

    Update framer.js

--- scripts/framer.js
+++ scripts/framer.js
@@ -1,20 +1,19 @@
 document.addEventListener('click', function (e) {
+    const form = document.querySelector(`form`);
     if (form) {
         const button = form.querySelector(`[type="submit"]`);
         const span = button.querySelector(`span`);
         const comment = form.querySelector(`#comment`);
-        const name = form.querySelector(`#name`);
-    }
-    if (e.target && e.target.innerText == 'Kirim') {
-        const form = document.querySelector(`form`);
-        if (comment) {
-            span.textContent = 'Batal';
+        if (e.target && e.target.innerText == 'Kirim') {
+            if (comment) {
+                span.textContent = 'Batal';
+            }
         }
-    }
-    else if (e.target && e.target.innerText == 'Batal') {
-        if (comment) {
-            form.reset();
-            span.textContent = 'Kirim';
+        else if (e.target && e.target.innerText == 'Batal') {
+            if (comment) {
+                form.reset();
+                span.textContent = 'Kirim';
+            }
         }
     }
 })
